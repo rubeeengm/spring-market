e2c41fe485573a72ac55b0bfdf7b590f08052d3d
Ruben Malaga <rubeeengm@outlook.com>
2022-04-09 20:18:10 -0500

    + se agrega documentación con swagger

--- src/main/java/com/proday/market/web/controller/ProductController.java
+++ src/main/java/com/proday/market/web/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.proday.market.web.controller;
 import com.proday.market.domain.Product;
 import com.proday.market.domain.service.ProductService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +30,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(
             this.productService.getAll(), HttpStatus.OK
@@ -33,7 +39,13 @@ public class ProductController {
     }
 
     @GetMapping("/{productId}")
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK")
+        , @ApiResponse(code = 404, message = "Product not found")
+    })
     public ResponseEntity<Product> getProduct(
+        @ApiParam(value = "The id of the product", required = true, example = "7")
         @PathVariable("productId") int productId
     ) {
         return this.productService.getProduct(productId)
